28f62f3a6bf9c02989d56aa6d4d7a1d155d90636
dariot <dario.turchetto@gmail.com>
2015-02-17 17:22:02 +0100

    sync

--- src/app.js
+++ src/app.js
@@ -5,6 +5,10 @@ app.use('/', express.static(__dirname + '/public'));
 app.use('/about', express.static(__dirname + '/public/about'));
 
 app.post('/', function(req, res) {
+    console.log(req.body.url);
+});
+
+app.get('/', function(req, res) {
     console.log(req);
 });
 
